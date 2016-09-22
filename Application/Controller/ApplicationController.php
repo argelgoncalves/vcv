1edc81fe3d41c5aeed85700d8eaa442e7a1dadf8
Argel Gonçalves <argel@localhost.(none)>
2016-09-22 20:24:56 -0300

    Adicionando comentarios

--- Application/Controller/ApplicationController.php
+++ Application/Controller/ApplicationController.php
@@ -5,6 +5,13 @@ namespace Application\Controller;
 use Application\Model\Auth;
 use Application\Model\MensagemSessao;
 
+/**
+ * Classe que contém todas as funções e variavéis necessárias a qualquer 
+ * Controller da aplicação, como validação de Autenticação, Mensagens, 
+ * Exibição de Layout, redirecionamento, etc.
+ * 
+ * Classe abstrata, ou seja não pode ser declarada
+ */
 abstract class ApplicationController {
 
     private $mensagemSessao;
@@ -13,6 +20,10 @@ abstract class ApplicationController {
     private $mensagensInfo;
     protected $atributos = array();
 
+    /**
+     * Inicializa as mensagens de Sessao, mensagens vindas de outras
+     * páginas e registra as mensagens de cada tipo na classe 
+     */
     public function __construct() {
         $this->mensagemSessao = new MensagemSessao();
         $this->mensagensErro = $this->mensagemSessao->getMensagensErro();
@@ -20,18 +31,59 @@ abstract class ApplicationController {
         $this->mensagensSucesso = $this->mensagemSessao->getMensagensSucesso();
     }
 
+    /**
+     * É utilizado para ler dados de propriedades inacessíveis. 
+     * 
+     *  Sobrecarga em PHP provê recursos para "criar" dinamicamente propriedades
+     *  e métodos. Estas entidades dinâmicas são processadas por métodos mágicos
+     *  fornecendo a uma classe vários tipos de ações.
+     *  Os métodos de sobrecarga são invocados ao interagir com 
+     *  propriedades ou métodos que não foram declarados ou não são visíveis no 
+     *  escopo corrente. O resto desta seção usará os termos 
+     * "propriedades inacessíveis" e "métodos inacessíveis" para referir-se a 
+     * esta combinação de declaração e visibilidade. 
+     * 
+     * 
+     * @param string $key propriedade a ser lida
+     * @return string
+     */
     public function __get($key) {
         return $this->atributos[$key];
     }
 
+    /**
+     *  É executado ao escrever dados em propriedades inacessíveis. 
+     * 
+     *  Sobrecarga em PHP provê recursos para "criar" dinamicamente propriedades
+     *  e métodos. Estas entidades dinâmicas são processadas por métodos mágicos
+     *  fornecendo a uma classe vários tipos de ações.
+     *  Os métodos de sobrecarga são invocados ao interagir com 
+     *  propriedades ou métodos que não foram declarados ou não são visíveis no 
+     *  escopo corrente. O resto desta seção usará os termos 
+     * "propriedades inacessíveis" e "métodos inacessíveis" para referir-se a 
+     *  esta combinação de declaração e visibilidade. 
+     * 
+     * 
+     * @param string $key Propriedade a ser lida
+     * @param string $value Valor da propriedade
+     */
     public function __set($key, $value) {
         $this->atributos[$key] = $value;
     }
 
+    /**
+     * Verificado se um formulário foi enviado à pagina
+     * @return boolean
+     */
     public function isPost() {
-        return count($this->getForm());
+        return count($this->getForm()) > 0;
     }
 
+    /**
+     * Retorna um array com os campos enviados pelo formulário, 
+     * sendo o índice do array é o nome do campo do formulario
+     * @return array
+     */
     public function getForm() {
         $form = array();
         foreach ($_POST as $key => $value) {
@@ -41,82 +93,166 @@ abstract class ApplicationController {
         return $form;
     }
 
+    /**
+     * Verifica se existe um indice enviado pela URL com o método GET
+     * @param string $index índice a ser testado
+     * @return boolean
+     */
     public function hasIndex($index) {
         return isset($_GET[$index]);
     }
 
+    /**
+     * Retorna o valor de um indice enviado pela URL com o método GET
+     * @param string index indice a ser obtido
+     * @return string valor do indice obtido
+     */
     public function getIndex($index) {
         return addslashes($_GET[$index]);
     }
 
+    /**
+     * Verifica se a sessão corrente está autenticada,
+     * caso não estiver, redireciona para a página de login
+     */
     public function validarSessao() {
         $auth = new Auth();
 
         if (!$auth->isLogado()) {
             $this->redirect("login/");
         }
-
-        return true;
     }
 
+    /**
+     * Redireciona a aplicação
+     * @param string $destino caminho do arquivo a ser redirecionado   
+     */
     public function redirect($destino) {
         header("location: /" . VIEW_PATH . $destino);
     }
 
+    /**
+     * Adiciona uma mensagem de erro para ser exibida na
+     * página atual
+     * @param string $mensagem Mensagem a ser registrada 
+     */
     public function addMensagemErro($mensagem) {
         array_push($this->mensagensErro, $mensagem);
     }
 
+    /**
+     * Adiciona uma mensagem de informação para ser exibida na
+     * página atual
+     * @param string $mensagem Mensagem a ser registrada 
+     */
     public function addMensagemInfo($mensagem) {
         array_push($this->mensagensInfo, $mensagem);
     }
 
+    /**
+     * Adiciona uma mensagem de sucesso para ser exibida na
+     * página atual
+     * @param string $mensagem Mensagem a ser registrada 
+     */
     public function addMensagemSucesso($mensagem) {
         array_push($this->mensagensSucesso, $mensagem);
     }
 
+    /**
+     * Adiciona uma mensagem de erro para ser exibida na
+     * próxima página
+     * @param string $mensagem Mensagem a ser registrada 
+     */
     public function addMensagemSessaoErro($mensagem) {
         $this->mensagemSessao->addMensagemErro($mensagem);
     }
 
+    /**
+     * Adiciona uma mensagem de informação para ser exibida na
+     * próxima página
+     * @param string $mensagem Mensagem a ser registrada 
+     */
     public function addMensagemSessaoInfo($mensagem) {
         $this->mensagemSessao->addMensagemInfo($mensagem);
     }
 
+    /**
+     * Adiciona uma mensagem de sucesso para ser exibida na
+     * próxima página
+     * @param string $mensagem Mensagem a ser registrada 
+     */
     public function addMensagemSessaoSucesso($mensagem) {
         $this->mensagemSessao->addMensagemSucesso($mensagem);
     }
 
+    /**
+     * Retorna as mensagens de erro da aplicação, caso
+     * não houver, retorna um array vazio
+     * @return array
+     */
     public function getMensagensErro() {
         return $this->mensagensErro;
     }
 
+    /**
+     * Retorna as mensagens de informação da aplicação, caso
+     * não houver, retorna um array vazio
+     * @return array mensagensInfo Mensagens de Informação
+     */
     public function getMensagensInfo() {
         return $this->mensagensInfo;
     }
 
+    /**
+     * Retorna as mensagens de sucesso da aplicação, caso
+     * não houver, retorna um array vazio
+     * @return array mensagensSucesso Mensagens de Sucesso
+     */
     public function getMensagensSucesso() {
         return $this->mensagensSucesso;
     }
 
+    /**
+     * Verifica se existem mensagens de erro registradas
+     * na aplicação
+     * @return boolean hasMensagensErro 
+     */
     public function hasMensagensErro() {
-        return count($this->mensagensErro);
+        return count($this->mensagensErro) > 0;
     }
 
+    /**
+     * Verifica se existem mensagens de informação registradas
+     * na aplicação
+     * @return boolean
+     */
     public function hasMensagensInfo() {
-        return count($this->mensagensInfo);
+        return count($this->mensagensInfo) > 0;
     }
 
+    /**
+     * Verifica se existem mensagens de sucesso registradas
+     * na aplicação
+     * @return boolean
+     */
     public function hasMensagensSucesso() {
-        return count($this->mensagensSucesso);
+        return count($this->mensagensSucesso) > 0;
     }
-    
-    public function displayHeader(){
-        include LAYOUT_PATH."_header.php";
+
+    /**
+     * Inclui e exibe o arquivo de cabeçalho do layout padrão da
+     * aplicação
+     */
+    public function displayHeader() {
+        include LAYOUT_PATH . "_header.php";
     }
-    
-    public function displayFooter(){
-        include LAYOUT_PATH."_footer.php";
+
+    /**
+     * Inclui e exibe o arquivo de rodapé do layout padrão da
+     * aplicação
+     */
+    public function displayFooter() {
+        include LAYOUT_PATH . "_footer.php";
     }
 
 }
--- Application/Model/DAO/AbstractDAO.php
+++ Application/Model/DAO/AbstractDAO.php
@@ -4,20 +4,42 @@ namespace Application\Model\DAO;
 
 use Application\Helper\ConexaoBD;
 
+/**
+ * Classe responsável pela manipulação de dados do banco
+ * Tem as funções para manipulação de um modo geral, sendo necessária 
+ * implementar as funções de insert e update 
+ */
 abstract class AbstractDAO {
 
     private $tableName;
     private $identifier;
 
+    /**
+     * Registra o nome e o identificador da tabela
+     * @param string $_tableName Nome da tabela
+     * @param string $_identifier Identificador da tabela
+     */    
     public function __construct($_tableName, $_identifier) {
         $this->tableName = $_tableName;
         $this->identifier = $_identifier;
     }
 
+    /**
+     * Insere um novo registro na tabela
+     * Necessário implementar
+     */
     abstract public function insert($object);
 
-    abstract function update($object);
+    /**
+     * Realiza uma alteração em um registro na tabela
+     * Necessário implementar
+     */
+    abstract public function update($object);
 
+    /**
+     * Deleta um registro da tabela
+     * @param int $id valor do identificador do registro
+     */
     public function delete($id) {
         $bd = new ConexaoBD();
         $bd->conectar();
@@ -33,14 +55,28 @@ abstract class AbstractDAO {
         return $status;
     }
 
+    /**
+     * Retorna o registro da tabela com o id desejado
+     * @param int $id valor do identificador do registro
+     * @return array
+     */
     public function selectById($id) {
         return $this->select($this->identifier . " = " . $id);
     }
 
+    /**
+     * Retorna todos os registros da tabela
+     * @return array
+     */
     public function selectAll() {
         return $this->select("1");
     }
 
+    /**
+     * Retorna todos os registros da tabela com a condiçao desejada
+     * @param string $where condicional da consulta
+     * @return array
+     */
     public function select($where) {
         $bd = new ConexaoBD();
         $bd->conectar();
@@ -62,6 +98,11 @@ abstract class AbstractDAO {
         return $array_results;
     }
 
+    /**
+     * Retorna q quantidade de registro da tabela
+     * @param string condicional da consulta
+     * @return int
+     */
     public function getCount($where = "1") {
         $bd = new ConexaoBD();
         $bd->conectar();
